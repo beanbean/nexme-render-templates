35fae4c8090d44bf1e15549403b3458a96934971
Cong Dau <dqcong@gmail.com>
2026-01-03 05:11:27 +0700

    thử thôi nha

---
+++ check_weight_workflow.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+API_KEY="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJiNjFmZmIxYy00ODNjLTQ3YjUtOTg5ZC1iYzJiNWQ1YjM3NzYiLCJpc3MiOiJuOG4iLCJhdWQiOiJwdWJsaWMtYXBpIiwiaWF0IjoxNzY3MjQ2NTA1fQ.wRd6jgQh73FXPWeW_Rn7b1UXXI_L8PGU_3JO6GcQOTg"
+
+curl -s "https://workflow.nexme.vn/api/v1/workflows/9fD7jTNV9LbMYGJu" \
+  -H "X-N8N-API-KEY: $API_KEY" | jq '{name, active, versionCounter}'
+//
